71e2cc1cfa09145587b58a6529c2f8db0c808000
Victor Figueroa <5786237+oceanskyweb@users.noreply.github.com>
2023-12-18 12:41:57 -0500

    Challenge completed

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,8 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
 const myString = 'developer';
+
+x = myString.slice(1); // Save letters started from second value
+myNewString = myString.charAt(0).toUpperCase() + x; // Developer
+
+myNewString = myString.charAt(0).toUpperCase() + myString.slice(1); // Developer
+console.log(myNewString);
